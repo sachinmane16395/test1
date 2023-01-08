25294b0c4c30c3157521cd8eacac522f16566f52
Sachin Mane <czx2816@varian.com>
2023-01-08 18:42:55 +0530

    hello

---
+++ src/main/java/com/example/aws/flight.java
@@ -0,0 +1,13 @@
+package com.example.aws;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class flight {
+    @GetMapping("/myflight")
+    public String getData(){
+        return "Please book your  Flight ticket, Choose Your Fav Airlines Idigo";
+    }
+
+}
